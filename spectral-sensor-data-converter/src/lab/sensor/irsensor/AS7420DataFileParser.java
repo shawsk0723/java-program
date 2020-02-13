a43f6580bee245b70e17a4a5680f8a7610118921
ivy-cho <wh5451@gmail.com>
2020-02-13 17:15:55 +0900

    Update AS7420DataFileParser.java
    AS7420 parser is added

--- spectral-sensor-data-converter/src/lab/sensor/irsensor/AS7420DataFileParser.java
+++ spectral-sensor-data-converter/src/lab/sensor/irsensor/AS7420DataFileParser.java
@@ -7,35 +7,95 @@ import java.util.List;
 public class AS7420DataFileParser implements ISensorDataFileParser {
 	
 	private static final String STR_WAVE_LENGTH = "Wavelength";
-//	private static final String STR_MEASURE_DATA = "Measure_data";
+	private static final String STR_MEASURE_DATA = "Measure data";
 	private static final String SEMICOLUMN = ";";
 	private static final int COLUMN_IDX_WAVELENGTH = 0;
 	private static final int COLUMN_IDX_DATA_REFLECTANCE = 1;
+	private static final int DATA_END_COUNT=125;
+	private static final int DATA_START_COUNT=56;
+	int count = 0;
 	
 
 	@Override
 	public List<String> getWaveLengthList(String sensorDataFilePath) {
-		return readRawDataByColumn(sensorDataFilePath, COLUMN_IDX_WAVELENGTH);
+		return readRawDataByraw(sensorDataFilePath, COLUMN_IDX_WAVELENGTH);
 	}
 
 	@Override
 	public List<String> getRawDataList(String sensorDataFilePath) {
-		return readRawDataByColumn(sensorDataFilePath, COLUMN_IDX_DATA_REFLECTANCE);
+		return readRawDataByraw(sensorDataFilePath, COLUMN_IDX_DATA_REFLECTANCE);
 	}
 
-	private List<String> readRawDataByColumn(String sensorDataFilePath, int columnIndex) {
+	private List<String> readRawDataByraw(String sensorDataFilePath, int columnIndex) {
 		List<String> rawDataList = new ArrayList<String>();
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(sensorDataFilePath));
+			
+		
 			String line = null;
+			if(columnIndex==COLUMN_IDX_WAVELENGTH)
+			{
 			while((line = br.readLine()) != null) {
 				if(line.contains(STR_WAVE_LENGTH)) {
+					
+					String[] lineSplit1 = line.split(SEMICOLUMN);
+					while(lineSplit1 != null) {
+						lineSplit1 = line.split(SEMICOLUMN);
+						if(count > DATA_START_COUNT && count < DATA_END_COUNT)
+						{
+						    System.out.printf("%s\n",lineSplit1[count]);
+						    rawDataList.add(lineSplit1[count]);
+						}
+						count++;
+						
+						if (count>DATA_END_COUNT)
+						{
+						    count=0;	
+							break;
+						}
+						
+					}
+					
+					
 					break;
 				}
 			}
-			while((line = br.readLine()) != null) {
-				String[] lineSplit = line.split(SEMICOLUMN);
-				rawDataList.add(lineSplit[columnIndex]);
+			}
+			else
+			{		
+				
+				while((line = br.readLine()) != null) {
+					
+					if(line.contains(STR_MEASURE_DATA)) {
+						System.out.println("hello world");	
+						if( count == 0)
+						{
+							line = br.readLine();
+							count++;
+							continue;
+						}
+						String[] lineSplit2 = line.split(SEMICOLUMN);
+						//count=0;
+						while(lineSplit2 != null) {
+							lineSplit2 = line.split(SEMICOLUMN);
+							if(count > DATA_START_COUNT && count < DATA_END_COUNT)
+							{
+							    System.out.printf("%s\n",lineSplit2[count]);
+							    rawDataList.add(lineSplit2[count]);
+							}
+							count++;
+							
+							if (count>DATA_END_COUNT)
+							{
+							    count=0;	
+								break;
+							}
+							
+						}
+						
+						break;
+					}
+				}
 			}
 			br.close();
 		} catch (IOException e) {
