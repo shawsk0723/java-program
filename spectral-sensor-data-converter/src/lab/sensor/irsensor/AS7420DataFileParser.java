23a9bf2a1c6d1f8b8f91a0d3a72c3f3efbc7a031
ivy-cho <wh5451@gmail.com>
2020-02-20 18:30:43 +0900

    Update AS7420DataFileParser.java

--- spectral-sensor-data-converter/src/lab/sensor/irsensor/AS7420DataFileParser.java
+++ spectral-sensor-data-converter/src/lab/sensor/irsensor/AS7420DataFileParser.java
@@ -1,20 +1,23 @@
 package lab.sensor.irsensor;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
 public class AS7420DataFileParser implements ISensorDataFileParser {
-	
-	private static final String STR_WAVE_LENGTH = "Wavelength";
-	private static final String STR_MEASURE_DATA = "Measure data";
+
+	private static final String STR_WAVE_LENGTH = "Wavelength [nm];;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;";
+	private static final String STR_MEASURE_DATA = "Measure data;";
 	private static final String SEMICOLUMN = ";";
+	private static final String WAVELENGTH_PARSING = ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;";
+	private static final String RAWDATA_PARSING = ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;";
+	private static final String DATA_END_POINT = ";;";
+
 	private static final int COLUMN_IDX_WAVELENGTH = 0;
 	private static final int COLUMN_IDX_DATA_REFLECTANCE = 1;
-	private static final int DATA_END_COUNT=125;
-	private static final int DATA_START_COUNT=56;
-	int count = 0;
-	
 
 	@Override
 	public List<String> getWaveLengthList(String sensorDataFilePath) {
@@ -30,85 +33,45 @@ public class AS7420DataFileParser implements ISensorDataFileParser {
 		List<String> rawDataList = new ArrayList<String>();
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(sensorDataFilePath));
-			
-		
+
 			String line = null;
-			if(columnIndex==COLUMN_IDX_WAVELENGTH)
-			{
-			while((line = br.readLine()) != null) {
-				if(line.contains(STR_WAVE_LENGTH)) {
-					
-					String[] lineSplit1 = line.split(SEMICOLUMN);
-					while(lineSplit1 != null) {
-						lineSplit1 = line.split(SEMICOLUMN);
-						if(count > DATA_START_COUNT && count < DATA_END_COUNT)
-						{
-						    System.out.printf("%s\n",lineSplit1[count]);
-						    rawDataList.add(lineSplit1[count]);
-						}
-						count++;
-						
-						if (count>DATA_END_COUNT)
-						{
-						    count=0;	
-							break;
-						}
-						
+
+			if (columnIndex == COLUMN_IDX_WAVELENGTH) {
+
+				while ((line = br.readLine()) != null) {
+					if (line.contains(STR_WAVE_LENGTH)) {
+						String[] lineSplit1 = line.split(WAVELENGTH_PARSING);
+						lineSplit1 = lineSplit1[1].split(DATA_END_POINT);
+						lineSplit1 = lineSplit1[0].split(SEMICOLUMN);
+						rawDataList = Arrays.asList(lineSplit1);
+						//System.out.println(rawDataList);
+						break;
 					}
-					
-					
-					break;
 				}
-			}
-			}
-			else
-			{		
-				
-				while((line = br.readLine()) != null) {
-					
-					if(line.contains(STR_MEASURE_DATA)) {
-						System.out.println("hello world");	
-						if( count == 0)
-						{
-							line = br.readLine();
-							count++;
-							continue;
-						}
-						String[] lineSplit2 = line.split(SEMICOLUMN);
-						//count=0;
-						while(lineSplit2 != null) {
-							lineSplit2 = line.split(SEMICOLUMN);
-							if(count > DATA_START_COUNT && count < DATA_END_COUNT)
-							{
-							    System.out.printf("%s\n",lineSplit2[count]);
-							    rawDataList.add(lineSplit2[count]);
-							}
-							count++;
-							
-							if (count>DATA_END_COUNT)
-							{
-							    count=0;	
-								break;
-							}
-							
-						}
-						
+
+			} else {
+
+				while ((line = br.readLine()) != null) {
+
+					if (line.contains(STR_MEASURE_DATA)) {
+
+						String[] lineSplit2 = line.split(RAWDATA_PARSING);
+						lineSplit2 = lineSplit2[1].split(DATA_END_POINT);
+						lineSplit2 = lineSplit2[0].split(SEMICOLUMN);
+						rawDataList = Arrays.asList(lineSplit2);
+						//System.out.println(rawDataList);
+
 						break;
 					}
 				}
 			}
+
 			br.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}		
-		return rawDataList;		
+		}
+		return rawDataList;
 	}
 
-	
-
 }
-
-
-
-
