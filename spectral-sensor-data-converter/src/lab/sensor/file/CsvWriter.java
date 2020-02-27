bffa3f8fb8027f894c9541739458b6fe49cd15b7
giwon.kang <giwon.kang@lge.com>
2020-02-27 10:19:09 +0900

    replace String with StringBuilder to improve String operation performance

--- spectral-sensor-data-converter/src/lab/sensor/file/CsvWriter.java
+++ spectral-sensor-data-converter/src/lab/sensor/file/CsvWriter.java
@@ -8,21 +8,23 @@ import java.io.IOException;
 import java.util.List;
 
 public class CsvWriter {
+	private static final String COMA = ",";
 	private File file;
-	
+		
 	public CsvWriter(String filePath) {
 		this.file = new File(filePath);
 	}
 
 	public void createAndWriteHeader(List<String> headers) throws Exception {
 		try {
-			String line = "";
+			StringBuilder line = new StringBuilder();
 			for(String header : headers) {
-				line = line + header + ",";
+				line.append(header);
+				line.append(COMA);
 			}
 
 			BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
-			bufferedWriter.write(line);
+			bufferedWriter.write(line.toString());
 			bufferedWriter.newLine();
 			bufferedWriter.close();
 		} catch (IOException e) {
@@ -32,13 +34,14 @@ public class CsvWriter {
 	
 	public void addRow(List<String> rowList) throws Exception {
 		try {
-			String line = "";
+			StringBuilder line = new StringBuilder();
 			for(String row : rowList) {
-				line = line + row + ",";
+				line.append(row);
+				line.append(COMA);
 			}
 
 			BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true));
-			bufferedWriter.append(line);
+			bufferedWriter.append(line.toString());
 			bufferedWriter.newLine();
 			bufferedWriter.close();
 		} catch (IOException e) {
